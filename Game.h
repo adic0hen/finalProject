b89e3e257417efddc8e4503b4a627e473e197ccf
adic0hen <38921986+adic0hen@users.noreply.github.com>
2018-08-16 12:44:08 +0300

    Update Game.h

--- Game.h
+++ Game.h
@@ -1,10 +1,5 @@
 #ifndef GAME_H_
 #define GAME_H_
-typedef struct Cell {
-	int solVal; /* the number that solve the current board, will change after every Backtracking */
-	int val; /* the number the user has entered. -1 if empty. val==solVal if isFixed */
-	int isFixed; /* 1 if the cell is fixed, 0 else */
-} cell; /*Cell structure is reffered to as -cell- */
 
 /*Board is internally represented as an array of m*n pointers to cells */
 #endif
