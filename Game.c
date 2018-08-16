69caa8273ebba986f3c30d9c609a771bcb6754a6
adic0hen <38921986+adic0hen@users.noreply.github.com>
2018-08-16 12:43:41 +0300

    Update Game.c

--- Game.c
+++ Game.c
@@ -1,2 +1,14 @@
 
+typedef struct cell {
+	int solVal; /* the number that solve the current board, will change after every Backtracking */
+	int val; /* the number the user has entered. -1 if empty. val==solVal if isFixed */
+	int isFixed; /* 1 if the cell is fixed, 0 else */
+} Cell; /*Cell structure is reffered to as -cell- */
+
+typedef struct node {
+	Cell* data;
+	struct node* next;
+	struct node* prev;
+} Node;
+
 
