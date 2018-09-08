f5dea6aa3aef5a4e833ac97dbe2a6e6d5cf92014
adic0hen <38921986+adic0hen@users.noreply.github.com>
2018-09-08 12:26:57 +0300

    adding init function (in gameplay module)

--- GamePlay.c
+++ GamePlay.c
@@ -9,11 +9,20 @@ int game() {
 	/*initialize buffer*/
 	char* buffer;
 	buffer = malloc(256); /*the allocation size is the same as the max length of a valid input*/
-
 	/*main game loop*/
 	while (mode != 0) {/*while mode is not "exit", if mode == reset than reset() is called and game starts over*/
 		printf("Enter your command:\n");
 		parse(buffer);
 	}
 	return 1;
+}
+
+void init() {
+	initiallizeGameParameters(9, 3, 3);
+	allocateMemForMainBoard();
+	initializeMainBoard();
+	allocateMemForLIFOCells();
+	initialUndoRedoListAndLIFOCells();
+	mode = 1;
+	markerrors = 0;
 }
